141152e1068081459c949b1382a4f1c2601731c4
WiZard-Leo <63330261+WiZard-Leo@users.noreply.github.com>
2022-02-24 21:42:15 +0800

    update readme

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+// 创建vue根实例
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// 加载全局样式
+import './styles/index.less'
 
 createApp(App).use(store).use(router).mount('#app')
